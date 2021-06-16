d3aec60b6f956a27c6b6817b6d7b665c35e65b0a
Tom Wilson <twilson63@gmail.com>
2021-06-16 12:23:14 +0000

    setup complete

--- adapter.js
+++ adapter.js
@@ -1,13 +1,31 @@
 const noop = () => null
 
-export function adapter(env) {
+// connectString = aws://key:secret@region/account
+// new Url(connectString)
+
+export function adapter({
+  prefix,
+  AWS_ACCOUNT,
+  AWS_REGION,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  SQSQueue
+}) {
+
   return Object.freeze({
+    // list queues
     index: noop,
+    // create queue
     create: noop,
+    // delete queue
     delete: noop,
+    // post job
     post: noop,
+    // get jobs
     get: noop,
+    // retry job
     retry: noop,
+    // cancel job
     cancel: noop
   })
 }
