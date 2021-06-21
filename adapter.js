39ac2965dba4a8d492a881259ed33e46a7b234a3
Tom Wilson <twilson63@gmail.com>
2021-06-21 21:12:32 +0000

    refactored aws lib

--- adapter.js
+++ adapter.js
@@ -1,7 +1,7 @@
 import { crocks, R } from './deps.js'
 
 const { Async } = crocks
-const { pluck } = R
+const { assoc, identity, pluck } = R
 
 const noop = () => null
 
@@ -10,25 +10,42 @@ export function adapter(svcName, aws) {
   const createBucket = Async.fromPromise(aws.createBucket)
   const createQueue = Async.fromPromise(aws.createQueue)
   const putObject = Async.fromPromise(aws.putObject)
+  const deleteQueue = Async.fromPromise(aws.deleteQueue)
+  const deleteBucket = Async.fromPromise(aws.deleteBucket)
+  const deleteObject = Async.fromPromise(aws.deleteObject)
 
   return Object.freeze({
     // list queues 
     index: getObject(svcName, 'queues').map(pluck('name')).toPromise(),
     // create queue
     create: ({ name, target, secret }) => {
-      return createBucket(svcName)
-        .chain(createQueue(svcName))
-        .chain(getObject(svcName, 'queues'))
+      return Async.of(svcName)
+        .chain(svcName => Async.all([
+          createBucket(svcName),
+          createQueue(svcName)
+        ]))
+        .chain(() => getObject(svcName, 'queues'))
         .bichain(
           () => putObject(svcName, 'queues', {}).map(() => ({})),
-          identity
+          Async.Resolved
         )
         .map(assoc(name, { target, secret }))
         .chain(queues => putObject(svcName, 'queues', queues))
         .toPromise()
     },
     // delete queue
-    delete: noop,
+    delete: name => getObject(svcName, 'queues')
+      .map(dissoc(name))
+      .chain(queues =>
+        keys(queues).length === 0
+          ? deleteObject(svcName, 'queues').chain(() => Async.all([
+            deleteQueue(svcName),
+            deleteBucket(svcName)
+          ]))
+          : putObject(svcName, 'queues', queues)
+      )
+      .toPromise()
+    ,
     // post job
     post: ({}),
     // get jobs
--- mod.js
+++ mod.js
@@ -1,15 +1,28 @@
 
 import { adapter } from './adapter.js'
-import { SQSQueue } from './deps.js'
+import createBucket from './lib/s3/create-bucket.js'
+import createQueue from './lib/sqs/create-queue.js'
+import putObject from './lib/s3/put-object.js'
+import getObject from './lib/s3/get-object.js'
+import deleteObject from './lib/s3/delete-object.js'
+import deleteQueue from './lib/sqs/delete-queue.js'
+import deleteBucket from './lib/s3/delete-bucket.js'
+import sendMessage from './lib/sqs/send-message.js'
+import receiveMessage from './lib/sqs/receive-message.js'
 
 /**
- * @param {string} config - url string aws://key:secret@region/account
+ * @param {string} svcName - name of queue service
  */
-export default function sqsAdapter(config) {
+export default function sqsAdapter(svcName) {
+  const aws = {
+    createBucket, createQueue, putObject,
+    getObject, deleteObject, deleteQueue, deleteBucket,
+    sendMessage, receiveMessage
+  }
   return Object.freeze({
     id: 'sqs',
     port: 'queue',
-    load: () => config,
-    link: env => () => adapter(env, SQSQueue)
+    load: () => svcName,
+    link: env => () => adapter(env, aws)
   })
 }
