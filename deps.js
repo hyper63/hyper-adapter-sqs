71653b15ddbdfb3b26b8f8e2f369c0eef27e92d3
Tom Wilson <twilson63@gmail.com>
2021-06-22 13:07:30 +0000

    got all of aws sqs methods wrapped

--- deps.js
+++ deps.js
@@ -1,3 +1,5 @@
 export { ApiFactory } from 'https://deno.land/x/aws_api/client/mod.ts';
 export { S3 } from 'https://deno.land/x/aws_api/services/s3/mod.ts'
 export { SQS } from 'https://deno.land/x/aws_api/services/sqs/mod.ts'
+export * as R from "https://cdn.skypack.dev/ramda@^0.27.1";
+export { default as crocks } from "https://cdn.skypack.dev/crocks@^0.12.4";
