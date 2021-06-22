1c615443a72712b82a5967f68359931a973a121e
Tom Wilson <twilson63@gmail.com>
2021-06-22 15:16:46 +0000

    added mock for create queue

--- adapter_test.js
+++ adapter_test.js
@@ -1,10 +1,12 @@
 import 'https://deno.land/x/dotenv@v2.0.0/load.ts'
 
+import * as aws from './aws-mock.js'
+/*
 import createBucket from './lib/s3/create-bucket.js'
 import createQueue from './lib/sqs/create-queue.js'
 import getObject from './lib/s3/get-object.js'
 import putObject from './lib/s3/put-object.js'
-
+*/
 /*
 import deleteObject from './lib/s3/delete-object.js'
 import deleteQueue from './lib/sqs/delete-queue.js'
@@ -17,24 +19,22 @@ import { assertEquals } from './deps_dev.js'
 import { adapter } from './adapter.js'
 
 const test = Deno.test
+/*
 const aws = {
   createBucket, createQueue, getObject, putObject
-  /*, putObject, getObject, deleteObject,
+  deleteObject,
   deleteQueue, deleteBucket, sendMessage, receiveMessage, deleteMessage
-  */
+  
 }
+*/
 
 const a = adapter('foobar', aws)
 
 test('create queue', async () => {
-
   const result = await a.create({
     name: 'baz',
     target: 'https://example.com',
     secret: 'secret'
   })
-  console.log(result)
-  //assertEquals(result.ok, true)
-  assertEquals(true, true)
-
+  assertEquals(result.ok, true)
 })
