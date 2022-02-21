963c4b19223dd831fa64c239fe16d1d84badf4a4
Tyler Hall <tyler@hyper.io>
2022-02-21 22:04:26 +0000

    fix!: resolve hyper error and bubble rejected non-handled exceptions #27
    BREAKING CHANGE - caught exceptions are resolved, not rejected, as hyper
    errors
    
    The only time a method on the adapter should return a rejected promise
    is in the case of an unhandled exception.
    
    Aligns with discussion detailed in:
    https://github.com/hyper63/journal/blob/master/design-docs/003-errors.md

--- deps_dev.js
+++ deps_dev.js
@@ -1,15 +1,7 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.123.0/testing/asserts.ts";
+  assertThrows,
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
 
-export { default as appOpine } from "https://x.nest.land/hyper-app-opine@1.2.8/mod.js";
-export { default as core } from "https://x.nest.land/hyper@2.0.0/mod.js";
-export { default as validateFactorySchema } from "https://x.nest.land/hyper@2.0.0/utils/plugin-schema.js";
-
-export {
-  adjectives,
-  animals,
-  colors,
-  uniqueNamesGenerator,
-} from "https://cdn.skypack.dev/unique-names-generator";
+export { default as validateFactorySchema } from "https://x.nest.land/hyper@2.1.1/utils/plugin-schema.js";
--- test/harness.js
+++ test/harness.js
@@ -1,14 +1,14 @@
 // Load .env
 import "https://deno.land/x/dotenv@v3.1.0/load.ts";
-
+import { default as appOpine } from "https://x.nest.land/hyper-app-opine@1.3.0/mod.js";
+import { default as core } from "https://x.nest.land/hyper@2.1.1/mod.js";
 import {
   adjectives,
   animals,
-  appOpine,
   colors,
-  core,
   uniqueNamesGenerator,
-} from "../deps_dev.js";
+} from "https://cdn.skypack.dev/unique-names-generator";
+
 import { default as myAdapter, PORT } from "../mod.js";
 
 const name = uniqueNamesGenerator({
