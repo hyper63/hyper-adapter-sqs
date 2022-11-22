5efcffc374a8324cd30893d099aeb6053984854c
Tyler Hall <tyler@hyper.io>
2022-11-22 22:29:02 +0000

    chore: bump dev deps

--- deps_dev.js
+++ deps_dev.js
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.153.0/testing/asserts.ts";
+} from "https://deno.land/std@0.165.0/testing/asserts.ts";
 
 export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.3.0/utils/plugin-schema.js";
