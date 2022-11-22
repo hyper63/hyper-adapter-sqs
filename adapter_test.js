ce8b28ad39797c597936dc7f7409b67a44d5904c
Tyler Hall <tyler@hyper.io>
2022-11-22 22:32:06 +0000

    test: refactor to use deno test step per module

--- adapter_test.js
+++ adapter_test.js
@@ -10,153 +10,154 @@ import { tokenErrs } from "./lib/utils.js";
 const test = Deno.test;
 const a = adapter({ name: "foobar", aws });
 
-test("* - should map AWS Token errors to HyperErr", async () => {
-  const original = aws.s3.getObject;
+test("adapter", async (t) => {
+  await t.step("* - should map AWS Token errors to HyperErr", async () => {
+    const original = aws.s3.getObject;
 
-  await Promise.all(
-    tokenErrs.map(async (te) => {
-      aws.s3.getObject = () => Promise.reject(new Error(`${te} - found`));
-      const a = adapter({ name: "foobar", aws });
-      await a.index().then((res) => {
-        assertEquals(res.ok, false);
-        assertEquals(res.status, 500);
-      });
-    }),
-  );
+    await Promise.all(
+      tokenErrs.map(async (te) => {
+        aws.s3.getObject = () => Promise.reject(new Error(`${te} - found`));
+        const a = adapter({ name: "foobar", aws });
+        await a.index().then((res) => {
+          assertEquals(res.ok, false);
+          assertEquals(res.status, 500);
+        });
+      }),
+    );
 
-  aws.s3.getObject = original;
-});
-
-test("create/delete queue", async () => {
-  const result = await a.create({
-    name: "baz",
-    target: "https://example.com",
-    secret: "secret",
-  });
-  assertEquals(result.ok, true);
-
-  // tear down
-  const cleanup = await a.delete("baz");
-  assertEquals(cleanup.ok, true);
-});
-
-test("don't create bucket if it exist", async () => {
-  let createCalled = false;
-  const original = aws.s3.checkBucket;
-  const originalCreateBucket = aws.s3.createBucket;
-  aws.s3.checkBucket = () => Promise.resolve(); // bucket already exists
-  aws.s3.createBucket = (name) => {
-    createCalled = true;
-    return originalCreateBucket(name);
-  };
-
-  const a = adapter({ name: "foobar", aws });
-
-  const result = await a.create({
-    name: "baz",
-    target: "https://example.com",
-    secret: "secret",
-  });
-  assertEquals(result.ok, true);
-  assertEquals(false, createCalled); // create should not be called
-
-  // tear down
-  const cleanup = await a.delete("baz");
-  assertEquals(cleanup.ok, true);
-  aws.s3.checkBucket = original;
-  aws.s3.createBucket = originalCreateBucket;
-});
-
-test("post a job to queue", async () => {
-  // setup
-  await a.create({
-    name: "test",
-    target: "https://jsonplaceholder.typicode.com/posts",
-  });
-  //console.log('x', x)
-  // post job
-  const result = await a.post({ name: "test", job: { hello: "world" } });
-  assertEquals(result.ok, true);
-
-  // tear down
-  await a.delete("test");
-});
-
-test("get error jobs from the queue", async () => {
-  // setup
-  await a.create({
-    name: "test2",
-    target: "https://jsonplaceholder.typicode.com/posts",
+    aws.s3.getObject = original;
   });
 
-  // test
-  const result = await a.get({ name: "test2", status: "ERROR" });
-  assertEquals(result.ok, true);
+  await t.step("create/delete queue", async () => {
+    const result = await a.create({
+      name: "baz",
+      target: "https://example.com",
+      secret: "secret",
+    });
+    assertEquals(result.ok, true);
 
-  // tear down
-  await a.delete("test2");
-});
-
-test("processed job race condition is handled", async () => {
-  const originalGetObject = aws.s3.getObject;
-  const originalListObjects = aws.s3.listObjects;
-
-  aws.s3.getObject = (_, name) => {
-    if (name.includes("not-found")) {
-      const err = new Error("NoSuchKey - woops");
-      err.code = "NoSuchKey";
-      return Promise.reject(err);
-    }
-
-    return Promise.resolve({ key: name, status: "READY" });
-  };
-
-  aws.s3.listObjects = () =>
-    Promise.resolve(["test2/key-1", "test2/not-found"]);
-  const a = adapter({ name: "foobar", aws });
-  // setup
-  await a.create({
-    name: "test2",
-    target: "https://jsonplaceholder.typicode.com/posts",
+    // tear down
+    const cleanup = await a.delete("baz");
+    assertEquals(cleanup.ok, true);
   });
 
-  // test
-  const result = await a.get({ name: "test2", status: "READY" });
-  assertEquals(result.ok, true);
-  assertEquals(result.jobs.length, 1);
+  await t.step("don't create bucket if it exist", async () => {
+    let createCalled = false;
+    const original = aws.s3.checkBucket;
+    const originalCreateBucket = aws.s3.createBucket;
+    aws.s3.checkBucket = () => Promise.resolve(); // bucket already exists
+    aws.s3.createBucket = (name) => {
+      createCalled = true;
+      return originalCreateBucket(name);
+    };
 
-  // tear down
-  await a.delete("test2");
-  aws.s3.getObject = originalGetObject;
-  aws.s3.listObjects = originalListObjects;
-});
+    const a = adapter({ name: "foobar", aws });
 
-test("retry an errored job from the queue", async () => {
-  // setup
-  await a.create({
-    name: "test2",
-    target: "https://jsonplaceholder.typicode.com/posts",
+    const result = await a.create({
+      name: "baz",
+      target: "https://example.com",
+      secret: "secret",
+    });
+    assertEquals(result.ok, true);
+    assertEquals(false, createCalled); // create should not be called
+
+    // tear down
+    const cleanup = await a.delete("baz");
+    assertEquals(cleanup.ok, true);
+    aws.s3.checkBucket = original;
+    aws.s3.createBucket = originalCreateBucket;
   });
 
-  // test
-  const result = await a.retry({ name: "test2", id: "1" });
-  assertEquals(result.ok, true);
+  await t.step("post a job to queue", async () => {
+    // setup
+    await a.create({
+      name: "test",
+      target: "https://jsonplaceholder.typicode.com/posts",
+    });
+    // post job
+    const result = await a.post({ name: "test", job: { hello: "world" } });
+    assertEquals(result.ok, true);
 
-  // tear down
-  await a.delete("test2");
-});
-
-test("cancel errored job from the queue", async () => {
-  // setup
-  await a.create({
-    name: "test2",
-    target: "https://jsonplaceholder.typicode.com/posts",
+    // tear down
+    await a.delete("test");
   });
 
-  // test
-  const result = await a.cancel({ name: "test2", id: "1" });
-  assertEquals(result.ok, true);
+  await t.step("get error jobs from the queue", async () => {
+    // setup
+    await a.create({
+      name: "test2",
+      target: "https://jsonplaceholder.typicode.com/posts",
+    });
 
-  // tear down
-  await a.delete("test2");
+    //await t.step
+    const result = await a.get({ name: "test2", status: "ERROR" });
+    assertEquals(result.ok, true);
+
+    // tear down
+    await a.delete("test2");
+  });
+
+  await t.step("processed job race condition is handled", async () => {
+    const originalGetObject = aws.s3.getObject;
+    const originalListObjects = aws.s3.listObjects;
+
+    aws.s3.getObject = (_, name) => {
+      if (name.includes("not-found")) {
+        const err = new Error("NoSuchKey - woops");
+        err.code = "NoSuchKey";
+        return Promise.reject(err);
+      }
+
+      return Promise.resolve({ key: name, status: "READY" });
+    };
+
+    aws.s3.listObjects = () =>
+      Promise.resolve(["test2/key-1", "test2/not-found"]);
+    const a = adapter({ name: "foobar", aws });
+    // setup
+    await a.create({
+      name: "test2",
+      target: "https://jsonplaceholder.typicode.com/posts",
+    });
+
+    // test
+    const result = await a.get({ name: "test2", status: "READY" });
+    assertEquals(result.ok, true);
+    assertEquals(result.jobs.length, 1);
+
+    // tear down
+    await a.delete("test2");
+    aws.s3.getObject = originalGetObject;
+    aws.s3.listObjects = originalListObjects;
+  });
+
+  await t.step("retry an errored job from the queue", async () => {
+    // setup
+    await a.create({
+      name: "test2",
+      target: "https://jsonplaceholder.typicode.com/posts",
+    });
+
+    // test
+    const result = await a.retry({ name: "test2", id: "1" });
+    assertEquals(result.ok, true);
+
+    // tear down
+    await a.delete("test2");
+  });
+
+  await t.step("cancel errored job from the queue", async () => {
+    // setup
+    await a.create({
+      name: "test2",
+      target: "https://jsonplaceholder.typicode.com/posts",
+    });
+
+    // test
+    const result = await a.cancel({ name: "test2", id: "1" });
+    assertEquals(result.ok, true);
+
+    // tear down
+    await a.delete("test2");
+  });
 });
