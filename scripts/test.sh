7c33f26d4298d0cc9ec46fa13fe1d521ffa9e234
Tyler Hall <tyler@hyper.io>
2022-11-22 22:28:50 +0000

    chore: do not create lockfile when running tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 deno lint
 deno fmt --check
-DENO_ENV=test deno test --unstable -A adapter_test.js process-tasks_test.js mod.test.js
+DENO_ENV=test deno test --unstable --no-lock -A adapter_test.js process-tasks_test.js mod.test.js
