998b901ad31c467f0cc76d96e600bde7389dade2
Tyler Hall <tyler@hyper.io>
2022-11-22 22:32:06 +0000

    test: refactor to use deno test step per module

--- mod.test.js
+++ mod.test.js
@@ -7,94 +7,96 @@ const { test } = Deno;
 // TODO: probably a better way to do this
 console.log = () => {};
 
-test("should be a valid schema", () => {
-  const factory = createFactory("foo");
-  assert(validateFactorySchema(factory));
-});
-
-test("load", async (t) => {
-  await t.step("load - should return the object", () => {
+test("mod", async (t) => {
+  await t.step("should be a valid schema", () => {
     const factory = createFactory("foo");
-    const res = factory.load();
-
-    assert(res.name);
-    assert(res.factory);
-    assert(res.aws);
-    assert(res.aws.s3);
-    assert(res.aws.sqs);
+    assert(validateFactorySchema(factory));
   });
 
-  await t.step("load - should use provided options", async () => {
-    const res = createFactory("foo", {
-      sleep: 1000,
-      concurrency: 15,
-      awsAccessKeyId: "foo",
-      awsSecretKey: "bar",
-      region: "fizz",
-    }).load();
+  await t.step("load", async (t) => {
+    await t.step("load - should return the object", () => {
+      const factory = createFactory("foo");
+      const res = factory.load();
 
-    await res.factory.ensureCredentialsAvailable();
-
-    assert(true);
-    assertEquals(res.sleep, 1000);
-    assertEquals(res.concurrency, 15);
-  });
-
-  await t.step("load - should use options passed to load", async () => {
-    const res = createFactory("foo").load({
-      sleep: 1000,
-      awsAccessKeyId: "foo",
-      awsSecretKey: "bar",
-      region: "fizz",
+      assert(res.name);
+      assert(res.factory);
+      assert(res.aws);
+      assert(res.aws.s3);
+      assert(res.aws.sqs);
     });
 
-    await res.factory.ensureCredentialsAvailable();
-    assert(true);
-    assertEquals(res.sleep, 1000);
-  });
-
-  await t.step(
-    "load - should merge options, preferring those passed to adapter",
-    async () => {
+    await t.step("load - should use provided options", async () => {
       const res = createFactory("foo", {
         sleep: 1000,
-        awsAccessKeyId: "better-id",
-      })
-        .load({
-          sleep: 2000,
-          awsAccessKeyId: "foo",
-          awsSecretKey: "bar",
-          region: "fizz",
-        });
+        concurrency: 15,
+        awsAccessKeyId: "foo",
+        awsSecretKey: "bar",
+        region: "fizz",
+      }).load();
 
       await res.factory.ensureCredentialsAvailable();
 
-      assertEquals(res.name, "foo");
-      assertEquals(res.awsAccessKeyId, "better-id");
+      assert(true);
       assertEquals(res.sleep, 1000);
-      assertEquals(res.awsSecretKey, "bar");
-      assertEquals(res.region, "fizz");
-    },
-  );
-
-  await t.step("load - should default the region to us-east-1", async () => {
-    const res = createFactory("foo").load({
-      awsAccessKeyId: "foo",
-      awsSecretKey: "bar",
+      assertEquals(res.concurrency, 15);
     });
 
-    await res.factory.ensureCredentialsAvailable();
+    await t.step("load - should use options passed to load", async () => {
+      const res = createFactory("foo").load({
+        sleep: 1000,
+        awsAccessKeyId: "foo",
+        awsSecretKey: "bar",
+        region: "fizz",
+      });
 
-    assertEquals(res.region, "us-east-1");
-  });
+      await res.factory.ensureCredentialsAvailable();
+      assert(true);
+      assertEquals(res.sleep, 1000);
+    });
 
-  await t.step("load - should default the sleep to 10000", () => {
-    const res = createFactory("foo").load();
-    assertEquals(res.sleep, 10000);
-  });
+    await t.step(
+      "load - should merge options, preferring those passed to adapter",
+      async () => {
+        const res = createFactory("foo", {
+          sleep: 1000,
+          awsAccessKeyId: "better-id",
+        })
+          .load({
+            sleep: 2000,
+            awsAccessKeyId: "foo",
+            awsSecretKey: "bar",
+            region: "fizz",
+          });
 
-  await t.step("load - should default the concurrency to 20", () => {
-    const res = createFactory("foo").load();
-    assertEquals(res.concurrency, 20);
+        await res.factory.ensureCredentialsAvailable();
+
+        assertEquals(res.name, "foo");
+        assertEquals(res.awsAccessKeyId, "better-id");
+        assertEquals(res.sleep, 1000);
+        assertEquals(res.awsSecretKey, "bar");
+        assertEquals(res.region, "fizz");
+      },
+    );
+
+    await t.step("load - should default the region to us-east-1", async () => {
+      const res = createFactory("foo").load({
+        awsAccessKeyId: "foo",
+        awsSecretKey: "bar",
+      });
+
+      await res.factory.ensureCredentialsAvailable();
+
+      assertEquals(res.region, "us-east-1");
+    });
+
+    await t.step("load - should default the sleep to 10000", () => {
+      const res = createFactory("foo").load();
+      assertEquals(res.sleep, 10000);
+    });
+
+    await t.step("load - should default the concurrency to 20", () => {
+      const res = createFactory("foo").load();
+      assertEquals(res.concurrency, 20);
+    });
   });
 });
--- process-tasks_test.js
+++ process-tasks_test.js
@@ -27,103 +27,105 @@ let receiveMessage = (url, count) =>
       '{"queue": "test", "target": "https://jsonplaceholder.typicode.com/posts", "job": {"hello": "world"}}',
   }]);
 
-test("receive messages", async () => {
-  asyncFetch = (target, options) => {
-    assertEquals(target, "https://jsonplaceholder.typicode.com/posts");
-    assertEquals(JSON.parse(options.body).hello, "world");
+test("process-tasks", async (t) => {
+  await t.step("receive messages", async () => {
+    asyncFetch = (target, options) => {
+      assertEquals(target, "https://jsonplaceholder.typicode.com/posts");
+      assertEquals(JSON.parse(options.body).hello, "world");
 
-    // no signature because no secret provided
-    assert(!options.headers["X-HYPER-SIGNATURE"]);
+      // no signature because no secret provided
+      assert(!options.headers["X-HYPER-SIGNATURE"]);
 
-    return Async.Resolved({ ok: true });
-  };
+      return Async.Resolved({ ok: true });
+    };
 
-  const result = await processTasks("foobar", asyncFetch, {
-    getQueueUrl,
-    receiveMessage,
-    deleteMessage,
-    putObject,
-    deleteObject,
-    log: logger(0),
-  })
-    .toPromise();
-  assertEquals(result[0].ok, true);
-});
+    const result = await processTasks("foobar", asyncFetch, {
+      getQueueUrl,
+      receiveMessage,
+      deleteMessage,
+      putObject,
+      deleteObject,
+      log: logger(0),
+    })
+      .toPromise();
+    assertEquals(result[0].ok, true);
+  });
 
-test("map token error to HyperErr", async () => {
-  const result = await processTasks("foobar", asyncFetch, {
-    getQueueUrl: () => Async.Rejected(new Error("ExpiredToken - found")),
-    receiveMessage,
-    deleteMessage,
-    putObject,
-    deleteObject,
-    log: logger(0),
-  })
-    .toPromise().catch((e) => e);
-  assertEquals(result.ok, false);
-  assertEquals(result.status, 500);
-});
+  await t.step("map token error to HyperErr", async () => {
+    const result = await processTasks("foobar", asyncFetch, {
+      getQueueUrl: () => Async.Rejected(new Error("ExpiredToken - found")),
+      receiveMessage,
+      deleteMessage,
+      putObject,
+      deleteObject,
+      log: logger(0),
+    })
+      .toPromise().catch((e) => e);
+    assertEquals(result.ok, false);
+    assertEquals(result.status, 500);
+  });
 
-test("failed to send to worker - mark job as error", async () => {
-  asyncFetch = () => {
-    return Async.Rejected(new Error("woops"));
-  };
+  await t.step("failed to send to worker - mark job as error", async () => {
+    asyncFetch = () => {
+      return Async.Rejected(new Error("woops"));
+    };
 
-  const result = await processTasks("foobar", asyncFetch, {
-    getQueueUrl,
-    receiveMessage,
-    deleteMessage,
-    putObject: R.curry(function (a, b, c) {
-      assertEquals(c.status, "ERROR");
-      assert(c.error);
-      return Async.fromPromise(s3.putObject)(a, b, c);
-    }),
-    deleteObject,
-    log: logger(0),
-  })
-    .toPromise();
-  assertEquals(result[0].ok, true);
-});
+    const result = await processTasks("foobar", asyncFetch, {
+      getQueueUrl,
+      receiveMessage,
+      deleteMessage,
+      putObject: R.curry(function (a, b, c) {
+        assertEquals(c.status, "ERROR");
+        assert(c.error);
+        return Async.fromPromise(s3.putObject)(a, b, c);
+      }),
+      deleteObject,
+      log: logger(0),
+    })
+      .toPromise();
+    assertEquals(result[0].ok, true);
+  });
 
-test("computes a signature", async () => {
-  receiveMessage = () =>
-    Async.Resolved([{
-      MessageId: "1",
-      ReceiptHandle: "1",
-      Body:
-        '{"queue": "test", "target": "https://jsonplaceholder.typicode.com/posts", "secret": "foobar", "job": {"hello": "world"}}',
-    }]);
+  await t.step("computes a signature", async () => {
+    receiveMessage = () =>
+      Async.Resolved([{
+        MessageId: "1",
+        ReceiptHandle: "1",
+        Body:
+          '{"queue": "test", "target": "https://jsonplaceholder.typicode.com/posts", "secret": "foobar", "job": {"hello": "world"}}',
+      }]);
 
-  asyncFetch = (target, options) => {
-    const header = options.headers["X-HYPER-SIGNATURE"];
-    assert(header);
+    asyncFetch = (target, options) => {
+      const header = options.headers["X-HYPER-SIGNATURE"];
+      assert(header);
 
-    const [first, second] = header.split(",");
-    assert(first);
-    assert(second);
+      const [first, second] = header.split(",");
+      assert(first);
+      assert(second);
 
-    const [, time] = first.split("t=");
-    const [, sig] = second.split("sig=");
+      const [, time] = first.split("t=");
+      const [, sig] = second.split("sig=");
 
-    assert(time);
-    assert(sig);
+      assert(time);
+      assert(sig);
 
-    assertEquals(
-      sig,
-      computeSignature("foobar", JSON.parse(options.body), time),
-    );
+      assertEquals(
+        sig,
+        computeSignature("foobar", JSON.parse(options.body), time),
+      );
 
-    return Async.Resolved({ ok: true });
-  };
+      return Async.Resolved({ ok: true });
+    };
 
-  const result = await processTasks("foobar", asyncFetch, {
-    getQueueUrl,
-    receiveMessage,
-    deleteMessage,
-    putObject,
-    deleteObject,
-    log: logger(0),
-  })
-    .toPromise();
-  assertEquals(result[0].ok, true);
+    const result = await processTasks("foobar", asyncFetch, {
+      getQueueUrl,
+      receiveMessage,
+      deleteMessage,
+      putObject,
+      deleteObject,
+      log: logger(0),
+    })
+      .toPromise();
+    assertEquals(result[0].ok, true);
+  });
 });
