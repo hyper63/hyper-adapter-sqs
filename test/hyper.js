f9fffdff373ad07f27bea2cac359c26548730119
Tyler Hall <tyler@hyper.io>
2023-11-28 19:58:17 -0500

    chore: bump dev deps and use npm in lieu of skypack

--- test/hyper.js
+++ test/hyper.js
@@ -1,7 +1,7 @@
 // Load .env
 import 'https://deno.land/x/dotenv@v3.1.0/load.ts'
-import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.0.2/packages/app-express/mod.ts'
-import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.0.1/packages/core/mod.ts'
+import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.1/packages/app-express/mod.ts'
+import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.2/packages/core/mod.ts'
 import {
   adjectives,
   animals,
